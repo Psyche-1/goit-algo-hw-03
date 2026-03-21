6a84a6336bb13d840a33ebbbc6431a6860b565f5
Volodymyr <68510557+Psyche-1@users.noreply.github.com>
2026-03-21 13:08:13 +0200

    Update get_numbers_ticket.py

--- get_numbers_ticket.py
+++ get_numbers_ticket.py
@@ -3,10 +3,10 @@ import random
 
 def get_numbers_ticket(min: int, max: int, quantity: int) -> list[int]:
     """Function return random numbers between min and max in quantity"""
-    if min < 1 or max > 1000:
+    if min < 1 or max > 1000 or max < min or (((max + 1) - min) < quantity):
         return []
 
-    return sorted(random.sample(range(min, max), quantity))
+    return sorted(random.sample(range(min, max + 1), quantity))
 
 
 # lottery_numbers = get_numbers_ticket(1, 49, 6)
